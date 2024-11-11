da4daf68f22e527a5eb6bed7b43eed92e3621cd8
HOLGUINUPC <you@example.com>
2024-11-10 20:54:10 -0500

    feat: add docker

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json({ limit: '1mb' })); 
+app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ limit: '1mb', extended: true }));
 
 dbConnect();
